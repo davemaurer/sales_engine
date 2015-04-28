484ec9b3359bdb3a40bb71e059a71abe15a83a51
David Maurer <davemaurermail@gmail.com>
2015-04-28 17:48:09 -0600

    added fixtures, along with list/search conditions for merchants and transactions. all tests passing.

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -4,12 +4,11 @@ class Merchant
               :created_at,
               :updated_at
 
-  def initialize(data, parent)
-    @parent = parent
+  def initialize(data, repository)
     @id = data[:id].to_i
     @name = data[:name]
     @created_at = data[:created_at]
     @updated_at = data[:updated_at]
+    @repository = repository
   end
-
 end
--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -8,27 +8,27 @@ require_relative 'transaction_repository'
 
 class SalesEngine
   attr_reader :customer_repository,
-              :invoice_repository
-              # :merchant_repository,
+              :invoice_repository,
+              :merchant_repository,
               # :item_repository,
               # :invoice_item_repository,
-              # :transaction_repository,
+              :transaction_repository
 
   def initialize(filepath) #we need to take in whatever we parse here
     @filepath = filepath
   end
 
   def startup     #and pass that into here. Then we send the relevant information down the right chain (ie. merchant = merchant chain)
-    customer_data            = Parser.parse("#{@filepath}/customers.csv")
-    @customer_repository     = CustomerRepository.new(customer_data, self)
-
-    # invoice_data        = Parser.parse("#{@filepath}/invoices.csv")
-    # @invoice_repository = InvoiceRepository.new(invoice_data, self)
-
-    # merchant_repository
-    # item_repository
-    # invoice_item_repository
-    # transaction_repository
+    customer_data             = Parser.parse("#{@filepath}/customers.csv")
+    @customer_repository      = CustomerRepository.new(customer_data, self)
+    # invoice_data            = Parser.parse("#{@filepath}/invoices.csv")
+    # @invoice_repository     = InvoiceRepository.new(invoice_data, self)
+    merchant_data             = Parser.parse("#{@filepath}/merchants.csv")
+    @merchant_repository      = MerchantRepository.new(merchant_data, self)
+    # item_repository         =
+    # invoice_item_repository =
+    transaction_data          = Parser.parse("#{@filepath}/transactions.csv")
+    @transaction_repository   = TransactionRepository.new(transaction_data, self)
   end
 
   def invoice_by_id(id)
